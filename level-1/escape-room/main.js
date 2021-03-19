4b49e9da6997ce0dd50f438a6ff1eeaab689e4dd
Blair Robbins <blairrobbins27@gmail.com>
2021-03-19 12:01:51 -0600

    added 3 = signs so the code is more accurate

--- level-1/escape-room/main.js
+++ level-1/escape-room/main.js
@@ -5,34 +5,34 @@ function playGame() {
 		"What's your move: 1. Put your hand in the hole 2. Find the key, or 3. Open the door (Please respond with a 1, 2, or 3): "
 	);
 
-	if (firstResponse == 1) {
+	if (firstResponse === 1) {
 		console.log(
 			"You have died. You really stuck your hand in a random dark hole? lawlz "
 		);
 		var diedResponse = readlineSync.question(
 			"Would you like to try agian? (y/n): "
 		);
-		if (diedResponse == "y") {
+		if (diedResponse === "y") {
 			playGame();
 		} else {
 			console.log("Okay bye... Quitter");
 		}
-	} else if (firstResponse == 2) {
+	} else if (firstResponse === 2) {
 		var secondResponse = readlineSync.question(
 			"You have found the key. Now what would you like to do? 1. Put your hand in the hole, or 3. Open the door: "
 		);
-		if (secondResponse == 3) {
+		if (secondResponse === 3) {
 			console.log(
 				"Congratulations! You have escaped the escape room! Yay.. Wahoo.. Go conquer the world now or something useful."
 			);
-		} else if (secondResponse == 1) {
+		} else if (secondResponse === 1) {
 			console.log(
 				"You died! Really? You thought sticking your hand in a random dark hole was a good idea? "
 			);
 			var diedResponse = readlineSync.question(
 				"Would you like to try agian? (y/n): "
 			);
-			if (diedResponse == "y") {
+			if (diedResponse === "y") {
 				playGame();
 			} else {
 				console.log("Okay bye... Quitter");
@@ -43,18 +43,18 @@ function playGame() {
 					secondResponse +
 					" is not a valid response. Would you like to try again? (y/n): "
 			);
-			if (invalidResponse == "y") {
+			if (invalidResponse === "y") {
 				playGame();
 			} else {
 				console.log("Okay bye... Quitter");
 			}
 		}
-	} else if (firstResponse == 3) {
+	} else if (firstResponse === 3) {
 		console.log("Sorry the door is locked! Maybe try finding the key first? ");
 		var lockedResponse = readlineSync.question(
 			"Would you like to try and find the key? (y/n): "
 		);
-		if (lockedResponse == "y") {
+		if (lockedResponse === "y") {
 			playGame();
 		} else {
 			console.log("Okay bye... Quitter");
@@ -65,7 +65,7 @@ function playGame() {
 				firstResponse +
 				" is not a valid response. Would you like to try again? (y/n): "
 		);
-		if (invalidResponse == "y") {
+		if (invalidResponse === "y") {
 			playGame();
 		} else {
 			console.log("Okay bye... Quitter");
