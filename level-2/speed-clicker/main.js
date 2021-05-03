a8f1fb09bfc85bade2120671fa68f579ef8af6d3
Blair Robbins <blairrobbins27@gmail.com>
2021-05-02 23:00:00 -0600

    final speed clicker

--- level-2/speed-clicker/main.js
+++ level-2/speed-clicker/main.js
@@ -1,18 +1,31 @@
 // This counts how many times a person clicks the webpage
 
-const currentNumber = document.getElementById(`number`)
-let count = localStorage.number || 0;
-currentNumber.textContent = localStorage.number
+
+// This gets the h1 and makes the current value equal to the local storage or 0
+const currentNumber = document.getElementById('number')
+currentNumber.textContent = localStorage.number || 0
+
+// This creates a new variable of count and sets it to the local storage number or 0
+let count = localStorage.number || 0
+
+
+function listening() {
+    document.addEventListener(`click`, function() {
+        // This takes either the local storage number or 0 and adds 1
+        count++
+
+        // This sets the currentNumber text content to the new number
+        currentNumber.textContent = count
+
+        // This stores the new number in the local storage
+        localStorage.setItem(`number`, count)
+    })
+}
+
+listening()
 
 
 
-
-document.addEventListener('click', function() {
-    count++
-    currentNumber.textContent = count
-    
-    localStorage.number = count
-})
 
 
 
