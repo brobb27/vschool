e1ec0ebf6ce3840198a1c530540199586340e63d
Blair Robbins <blairrobbins27@gmail.com>
2021-08-17 17:26:01 -0600

    changed all classes to useState instead

--- level-3/dj-react/src/App.js
+++ level-3/dj-react/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Main from './components/Main'
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            color: ['white', 'white', 'white', 'white']
-        }
-    }
-
-    render() {
-        return (
+function App() {
+    return (
+        <div>
             <Main />
-        )
-    }
+        </div>
+    )
 }
 
 export default App
