34bd5660c8c0ce8cb1f6fc4c6cf8bf803755004d
Blair Robbins <blairrobbins27@gmail.com>
2021-08-18 10:45:17 -0600

    refactored name badge assignment using hooks and functional components

--- level-3/name-badge/src/App.js
+++ level-3/name-badge/src/App.js
@@ -1,152 +1,299 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Badge from './Badge'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      badge: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        birthPlace: '',
-        phone: '',
-        favFood: '',
-        aboutYou: '',
-        randomColor: ''
-      },
-      badgeList: []
+function App() {
+  const [badge, setBadge] = useState(
+    {
+      firstName: '',
+      lastName: '',
+      email: '',
+      birthPlace: '',
+      phone: '',
+      favFood: '',
+      aboutYou: '',
+      randomColor: ''
     }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+  )
+  const [badgeList, setBadgeList] = useState([])
+  
+  // Handles any change made on the badge to reflect the user input
+  function handleChange(e) {
+    const {name, value} = e.target
 
-  handleChange(event) {
-    const {name, value} = event.target
-    this.setState(prevState => {
+    setBadge(prevBadge => {
       return {
-        badge: {
-          ...prevState.badge,
-          [name]: value
-        }
+        ...prevBadge,
+        [name]: value
       }
     })
   }
 
-  handleSubmit(event) {
-    event.preventDefault()
+  // Handles form submition and adds the badge to the list
+  function handleSubmit(e) {
+    e.preventDefault()
     
-    this.setState(prevState => {
-      const updatedList = [...prevState.badgeList, this.state.badge]
-
-      return {
-        badge: {
-          firstName: '',
-          lastName: '',
-          email: '',
-          birthPlace: '',
-          phone: '',
-          favFood: '',
-          aboutYou: '',
-          randomColor: ''
-        },
-        badgeList: updatedList,
-      }
+    setBadgeList(prevBadgeList => {
+      return (
+        [...prevBadgeList, badge]
+      )
     })
-}
 
+    setBadge({
+      firstName: '',
+      lastName: '',
+      email: '',
+      birthPlace: '',
+      phone: '',
+      favFood: '',
+      aboutYou: '',
+      randomColor: ''
+    })
 
-  render() {
-    const badgeComponents = this.state.badgeList.map(badge => <Badge key={badge.phone} info={badge} />)
-    return (
-      <div id='main'>
-        <div id='badgeForm'>
-          <form onSubmit={this.handleSubmit}>
-            <div id='basicInfo'>
-              <input
-                type='text'
-                name='firstName'
-                value={this.state.badge.firstName}
-                placeholder='First Name'
-                minLength='3'
-                required
-                onChange={this.handleChange}
-              />
-
-              <input
-                type='text'
-                name='lastName'
-                value={this.state.badge.lastName}
-                placeholder='Last Name'
-                minLength='3'
-                required
-                onChange={this.handleChange}
-              />
-
-              <input
-                type='email'
-                name='email'
-                value={this.state.badge.email}
-                placeholder='Email'
-                minLength='3'
-                required
-                onChange={this.handleChange}
-              />
-
-              <input
-                type='text'
-                name='birthPlace'
-                value={this.state.badge.birthPlace}
-                placeholder='Place of Birth'
-                minLength='3'
-                required
-                onChange={this.handleChange}
-              />
-
-              <input
-                type='tel'
-                name='phone'
-                pattern="[0-9]{10}"
-                value={this.state.badge.phone}
-                placeholder='Phone'
-                minLength='10'
-                required
-                onChange={this.handleChange}
-              />
-
-              <input
-                type='text'
-                name='favFood'
-                value={this.state.badge.favFood}
-                placeholder='Favorite Food'
-                minLength='3'
-                required
-                onChange={this.handleChange}
-              />
-            </div>
-
-            <textarea
-                id="aboutYou"
-                type='textArea'
-                name='aboutYou'
-                value={this.state.badge.aboutYou}
-                placeholder='Tell us about yourself'
-                minLength='3'
-                required
-                onChange={this.handleChange}
-            />
-            <br />
-
-            <button>Submit</button>
-            
-          </form>
-        </div>
-          {badgeComponents}
-      </div>
-    )
   }
 
+  const badgeComponents = badgeList.map(badge => <Badge key={badge.phone} info={badge} />)
+
+  return (
+    <div id='main'>
+      <div id='badgeForm'>
+        <form onSubmit={handleSubmit}>
+          <div id='basicInfo'>
+            <input
+              type='text'
+              name='firstName'
+              value={badge.firstName}
+              placeholder='First Name'
+              minLength='3'
+              required
+              onChange={handleChange}
+            />
+
+            <input
+              type='text'
+              name='lastName'
+              value={badge.lastName}
+              placeholder='Last Name'
+              minLength='3'
+              required
+              onChange={handleChange}
+            />
+
+            <input
+              type='email'
+              name='email'
+              value={badge.email}
+              placeholder='Email'
+              minLength='3'
+              required
+              onChange={handleChange}
+            />
+
+            <input
+              type='text'
+              name='birthPlace'
+              value={badge.birthPlace}
+              placeholder='Place of Birth'
+              minLength='3'
+              required
+              onChange={handleChange}
+            />
+
+            <input
+              type='tel'
+              name='phone'
+              pattern="[0-9]{10}"
+              value={badge.phone}
+              placeholder='Phone'
+              minLength='10'
+              required
+              onChange={handleChange}
+            />
+
+            <input
+              type='text'
+              name='favFood'
+              value={badge.favFood}
+              placeholder='Favorite Food'
+              minLength='3'
+              required
+              onChange={handleChange}
+            />
+          </div>
+
+          <textarea
+              id="aboutYou"
+              type='textArea'
+              name='aboutYou'
+              value={badge.aboutYou}
+              placeholder='Tell us about yourself'
+              minLength='3'
+              required
+              onChange={handleChange}
+          />
+          <br />
+
+          <button>Submit</button>
+          
+        </form>
+      </div>
+      {badgeComponents}
+    </div>
+  )
+
 }
 
+export default App
 
-export default App;
+
+
+// Class based component
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       badge: {
+//         firstName: '',
+//         lastName: '',
+//         email: '',
+//         birthPlace: '',
+//         phone: '',
+//         favFood: '',
+//         aboutYou: '',
+//         randomColor: ''
+//       },
+//       badgeList: []
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//   }
+
+//   handleChange(event) {
+//     const {name, value} = event.target
+//     this.setState(prevState => {
+//       return {
+//         badge: {
+//           ...prevState.badge,
+//           [name]: value
+//         }
+//       }
+//     })
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault()
+    
+//     this.setState(prevState => {
+//       const updatedList = [...prevState.badgeList, this.state.badge]
+
+//       return {
+//         badge: {
+//           firstName: '',
+//           lastName: '',
+//           email: '',
+//           birthPlace: '',
+//           phone: '',
+//           favFood: '',
+//           aboutYou: '',
+//           randomColor: ''
+//         },
+//         badgeList: updatedList,
+//       }
+//     })
+// }
+
+
+//   render() {
+//     const badgeComponents = this.state.badgeList.map(badge => <Badge key={badge.phone} info={badge} />)
+//     return (
+//       <div id='main'>
+//         <div id='badgeForm'>
+//           <form onSubmit={this.handleSubmit}>
+//             <div id='basicInfo'>
+//               <input
+//                 type='text'
+//                 name='firstName'
+//                 value={this.state.badge.firstName}
+//                 placeholder='First Name'
+//                 minLength='3'
+//                 required
+//                 onChange={this.handleChange}
+//               />
+
+//               <input
+//                 type='text'
+//                 name='lastName'
+//                 value={this.state.badge.lastName}
+//                 placeholder='Last Name'
+//                 minLength='3'
+//                 required
+//                 onChange={this.handleChange}
+//               />
+
+//               <input
+//                 type='email'
+//                 name='email'
+//                 value={this.state.badge.email}
+//                 placeholder='Email'
+//                 minLength='3'
+//                 required
+//                 onChange={this.handleChange}
+//               />
+
+//               <input
+//                 type='text'
+//                 name='birthPlace'
+//                 value={this.state.badge.birthPlace}
+//                 placeholder='Place of Birth'
+//                 minLength='3'
+//                 required
+//                 onChange={this.handleChange}
+//               />
+
+//               <input
+//                 type='tel'
+//                 name='phone'
+//                 pattern="[0-9]{10}"
+//                 value={this.state.badge.phone}
+//                 placeholder='Phone'
+//                 minLength='10'
+//                 required
+//                 onChange={this.handleChange}
+//               />
+
+//               <input
+//                 type='text'
+//                 name='favFood'
+//                 value={this.state.badge.favFood}
+//                 placeholder='Favorite Food'
+//                 minLength='3'
+//                 required
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+
+//             <textarea
+//                 id="aboutYou"
+//                 type='textArea'
+//                 name='aboutYou'
+//                 value={this.state.badge.aboutYou}
+//                 placeholder='Tell us about yourself'
+//                 minLength='3'
+//                 required
+//                 onChange={this.handleChange}
+//             />
+//             <br />
+
+//             <button>Submit</button>
+            
+//           </form>
+//         </div>
+//           {badgeComponents}
+//       </div>
+//     )
+//   }
+
+// }
+
+
+// export default App;
--- level-3/name-badge/src/Badge.js
+++ level-3/name-badge/src/Badge.js
@@ -1,49 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import randomcolor from 'randomcolor'
 import axios from 'axios'
 
-class Badge extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            randomColor: ''
-        }
-    }
+function Badge(props) {
+    const [randomColor, setColor] = useState(getColor)
 
-    // componentDidMount() {
-    //     const newColor = randomcolor()
-    //     console.log(newColor)
-    //     this.setState({
-    //         randomColor: newColor
-    //     })
-    // }
-
-    // when the component mounts it sets the state of random color to the new retrieved color
-    componentDidMount() {
+    function getColor() {
         axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
-            .then(res => this.setState({
-                randomColor: res.data.colors[0].hex
-            }))
-            .catch(err => console.log(err))
+        .then(res => setColor(() => {
+            return (
+                res.data.colors[0].hex
+            )
+        }))
+        .catch(err => console.log(err))
     }
 
-    render() {
-        return (
-            <div id='userBadgeContainer'>
-                <div id='badgeHeader' style={{backgroundColor: `#${this.state.randomColor}`}}>
-                    <h2>Badge:</h2>
-                </div>
-                <div id='userBadgeInfo'>
-                    <p>Name: {this.props.info.firstName} {this.props.info.lastName}</p>
-                    <p>Phone: {this.props.info.phone}</p>
-                    <p>Place of Birth: {this.props.info.birthPlace}</p>
-                    <p>Favorite Food: {this.props.info.favFood}</p>
-                    <p>Email: {this.props.info.email}</p>
-                </div>
-                <p id='userAboutYou'>{this.props.info.aboutYou}</p>
+    return (
+        <div id='userBadgeContainer'>
+            <div id='badgeHeader' style={{backgroundColor: `#${randomColor}`}}>
+                <h2>Badge:</h2>
             </div>
-        )
-    }
+            <div id='userBadgeInfo'>
+                <p>Name: {props.info.firstName} {props.info.lastName}</p>
+                <p>Phone: {props.info.phone}</p>
+                <p>Place of Birth: {props.info.birthPlace}</p>
+                <p>Favorite Food: {props.info.favFood}</p>
+                <p>Email: {props.info.email}</p>
+            </div>
+            <p id='userAboutYou'>{props.info.aboutYou}</p>
+        </div>
+    )
 }
 
 export default Badge
+
+// Old react code
+
+// class Badge extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             randomColor: ''
+//         }
+//     }
+
+//     // componentDidMount() {
+//     //     const newColor = randomcolor()
+//     //     console.log(newColor)
+//     //     this.setState({
+//     //         randomColor: newColor
+//     //     })
+//     // }
+
+//     // when the component mounts it sets the state of random color to the new retrieved color
+//     componentDidMount() {
+//         axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+//             .then(res => this.setState({
+//                 randomColor: res.data.colors[0].hex
+//             }))
+//             .catch(err => console.log(err))
+//     }
+
+//     render() {
+//         return (
+//             <div id='userBadgeContainer'>
+//                 <div id='badgeHeader' style={{backgroundColor: `#${this.state.randomColor}`}}>
+//                     <h2>Badge:</h2>
+//                 </div>
+//                 <div id='userBadgeInfo'>
+//                     <p>Name: {this.props.info.firstName} {this.props.info.lastName}</p>
+//                     <p>Phone: {this.props.info.phone}</p>
+//                     <p>Place of Birth: {this.props.info.birthPlace}</p>
+//                     <p>Favorite Food: {this.props.info.favFood}</p>
+//                     <p>Email: {this.props.info.email}</p>
+//                 </div>
+//                 <p id='userAboutYou'>{this.props.info.aboutYou}</p>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Badge
