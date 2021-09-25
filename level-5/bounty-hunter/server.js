87e121c6fb6baa4738fb304b0723f2352ada8380
Blair Robbins <blairrobbins27@gmail.com>
2021-09-24 23:20:40 -0600

    first commit of bounty hunter application

--- level-5/bounty-hunter/server.js
+++ level-5/bounty-hunter/server.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 
+// Middleware
+app.use(express.json()) // looks for a request body
 
-app.get('/homie', (req, res) => {
-    res.send({name: 'joe', age: 27})
-})
+// Routes
+app.use('/bounties', require('./routes/bountyRouter'))
 
-
+// Server listening
 app.listen(7000, () => {
     console.log('Server is up and running on port 7000')
 })
