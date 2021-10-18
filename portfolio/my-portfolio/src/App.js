03ad2b71d33d5ad37579e374637c57cb2e3f13b7
Blair Robbins <blairrobbins27@gmail.com>
2021-10-18 16:08:39 -0600

    final styling of portfolio

--- portfolio/my-portfolio/src/App.js
+++ portfolio/my-portfolio/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Header from './components/Header'
 import AboutMe from './components/AboutMe'
 import Projects from './components/Projects'
+import ContactFooter from './components/ContactFooter'
 
 function App() {
   return (
@@ -11,8 +12,7 @@ function App() {
       <Header />
       <AboutMe />
       <Projects />
-      <h1>Contact Info/Resume</h1>
-      <h1>Footer</h1>
+      <ContactFooter />
     </div>
   );
 }
--- portfolio/my-portfolio/src/components/Navbar.js
+++ portfolio/my-portfolio/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import personalLogo from '../images/site_logo.png'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 function Navbar() {
     // state for the background color
     const [scrolled, setScroll] = useState(false)
+    const [isDropped, setDrop] = useState(false)
 
     // function to change state on scroll
     function changeBackground() {
@@ -17,17 +19,25 @@ function Navbar() {
     // event listener to trigger function
     window.addEventListener('scroll', changeBackground)
 
+    //dropdown true or false?
+    function dropdown() {
+        setDrop(prevState => !prevState)
+    }
+
     return (
         <div id='navbar' className={scrolled ? 'scroll' : ''}>
             <div id='navbarTitle'>
                 <img src={personalLogo} alt='Personal Logo'/>
                 <a href='header'>Blair Robbins</a>
             </div>
-            <div id='navbarLinks'>
+            <div className={isDropped === false ? 'navbarLinks' : 'droppedOptions'}>
+                <div id='dropper' onClick={dropdown}>
+                    <GiHamburgerMenu />
+                </div>
                 <a href='#header'>Home</a>
                 <a href='#aboutMe'>About Me</a>
                 <a href='#projects'>Projects</a>
-                <a href='#contactMe'>Contact Me</a>
+                <a href='#contact'>Contact Me</a>
             </div>
         </div>
     )
